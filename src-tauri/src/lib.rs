e792a7dd1cade72d8c751e8cfa4590da1a4e850c
rodrfct <rodrigoca.fct@gmail.com>
2025-10-10 16:14:51 +0200

    Add license

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,22 @@
-use tauri::{ AppHandle, Manager };
-use tauri_plugin_dialog::{ DialogExt, FilePath };
+// Copyright (C) 2025 rodrfct <rodrigoca.fct@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
+use tauri::{AppHandle, Manager};
+use tauri_plugin_dialog::{DialogExt, FilePath};
 
 #[tauri::command]
 async fn pick_file(app: AppHandle) -> Option<FilePath> {
